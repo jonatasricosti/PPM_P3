b28eb0479fd02fc02805707b76f7775f81f766db
jonatasricosti <jonatas-ricosti@hotmail.com>
2026-02-25 00:01:15 -0300

    adicionado função pra desenhar pixel

--- main.cpp
+++ main.cpp
@@ -1,28 +1,67 @@
-#include <fstream>
+#include <fstream> // pra usar ofstream
+#include <sstream> // pra usar stringstream
+#include <string> // pra usar string
 using namespace std;
 
+string FileType = "P3";	
+const int image_width = 640;
+const int image_height = 480;
+const int magic_number = 255;
 
-void CreatImage()
+
+// use essa função pra conveter uma cor em rgb24bits de hexadecimal pra decimal
+string rgb24Todecimal(int color)
+{
+	int red = color >> 16 & 255;
+	int green = color >> 8 & 255;
+	int blue = color & 255;
+	
+	stringstream escreva;
+	escreva << red << " " << green << " " << blue << endl;
+	string texto = escreva.str();
+	
+	return texto;
+}
+
+
+// Esse array vai armazenar todas as nossas cores
+// Esse array simula a memória de vídeo do game boy advance no mode 3
+int vram[image_width*image_height];
+
+// use essa função pra desenhar pixel
+void DrawPixel(int x, int y, int color)
+{
+	vram[x+y*image_width] = color;
+}
+
+
+// use essa função pra colocar a cor de fundo da imagem
+void BackgroundColor(int color)
+{
+	for(int i = 0; i < image_width*image_height; i++)
+	{
+		vram[i] = color; // cor de fundo
+	}
+}
+
+// Use essa função pra criar um arquivo de imagem ppm p3
+void CreatImagePP3()
 {
 	ofstream escreva;
 	escreva.open("text.ppm");
 	
-	const int image_width = 640;
-	const int image_height = 480;
-	const int magic_number = 255;
-	
-	int red = 0;
-	int green = 0;
-	int blue = 255;
-		
-	escreva << "P3" << endl;
+	escreva << FileType << endl;
 	escreva << image_width << endl;
 	escreva << image_height << endl;
 	escreva << magic_number << endl;
 	
+	BackgroundColor(0xffff00);
+	
+	DrawPixel(100,200,0);
+	
 	for(int i = 0; i < image_width*image_height; i++)
 	{
-		escreva << red << " " << green << " " << blue << " " << endl;
+		escreva << rgb24Todecimal(vram[i]);		
 	}
 	
 	escreva.close();
@@ -31,7 +70,7 @@ void CreatImage()
 int main()
 {
 
-CreatImage();
+CreatImagePP3();
 
 return 0;
 }
